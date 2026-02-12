1d77c93033784052d90ab9539777e185f40e742a
ShyamAshram <shyamashram@gmail.com>
2026-02-11 19:16:52 -0500

    HASROUTER

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
